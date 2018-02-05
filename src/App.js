f813aa9518f9d8a309dacdc5e03f19ad09122df6
Alexander For Example <makeev@ecookna.ru>
2018-02-05 17:09:53 +0300

    HelloWorld Input

--- src/App.js
+++ src/App.js
@@ -3,27 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 
 class HelloWorld extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            displayedText: 'stranger'
+        };
+    }
+
+    handleNameChange(event) {
+        this.setState({
+            displayedText: event.target.value
+        });
+    }
+
     render() {
-        const { props: { title, author, body } } = this;
-        return (<div className="State">
-            <h3>{title}</h3>
-            <h4>{author}</h4>
-            <h5>{body}</h5>
-        </div>)
+        return (
+            <div>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    className="search-field"
+                    onChange={this.handleNameChange.bind(this)}>
+                </input>
+                <div>
+                    Hello, {this.state.displayedText}
+                </div>
+            </div>
+        );
     }
 }
 
 class App extends Component {
     render() {
-        return (<div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <h1 className="App-title">Makeev vs React</h1>
-            </header>
-            <p className="App-intro">
-                <HelloWorld title="Заголовок" author="Автор" body="Статья"/>
-            </p>
-        </div>);
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Makeev vs React</h1>
+                </header>
+                <div className="App-intro">
+                    <HelloWorld />
+                </div>
+            </div>
+        );
     }
 }
 // To get started, edit <code>src/App.js</code> and save to reload.
