05565288337725a90762fffff026121fb7e24a61
Alexander For Example <makeev@ecookna.ru>
2018-02-06 11:09:16 +0300

    ContactList Updated

--- src/App.js
+++ src/App.js
@@ -8,42 +8,84 @@ const CONTACTS = [
         name: 'Darth Vader',
         phoneNumber: '+250966666666',
         image: 'img/darth.gif',
+        email: 'darthvader@sith.net',
+        address: 'DeathStar',
     }, {
         id: 2,
         name: 'Princess Leia',
         phoneNumber: '+250966344466',
         image: 'img/leia.gif',
+        email: 'leiaorgana@rebel.net',
+        address: 'Hot',
     }, {
         id: 3,
         name: 'Luke Skywalker',
         phoneNumber: '+250976654433',
         image: 'img/luke.gif',
+        email: 'lukeskywalker@rebel.net',
+        address: 'Tatuin',
     }, {
         id: 4,
         name: 'Chewbacca',
         phoneNumber: '+250456784935',
         image: 'img/chewbacca.gif',
+        email: 'chewbacca@archer.net',
+        address: 'Kashiik',
     }, {
         id: 5,
         name: 'R2D2',
         phoneNumber: '+232223233',
-        image: 'img/',
+        image: 'img/r2d2.gif',
+        email: 'R2D2@driods.net',
+        address: 'DroidSity',
     },
 ];
 
 class Contact extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpened: false
+        };
+    }
+
+    hadnleClick = () => {
+        let {isOpened} = this.state;
+        isOpened ? this.setState({
+            isOpened: false
+        }) :
+        this.setState({
+            isOpened: true
+        });
+        console.log(isOpened);
+    }
+
     render() {
-        return (<li className="contact">
-            <img className="contact-image" src={this.props.image} alt="logo" width="60px" height="60px"/>
-            <div className="contact-info">
-                <div className="contact-name">
-                    {this.props.name}
-                </div>
-                <div className="contact-number">
-                    {this.props.phoneNumber}
-                </div>
-            </div>
-        </li>);
+        const {image, name, phoneNumber, email, address} = this.props;
+        const {isOpened} = this.state;
+        return  <li className="contact" onClick={this.hadnleClick}>
+                    <img className="contact-image"
+                        src={image}
+                        alt="logo" width="60px"
+                        height="60px"/>
+                    <div className="contact-info">
+                        <div className="contact-name">
+                            {name}
+                        </div>
+                        <div className="contact-number">
+                            {phoneNumber}
+                        </div>
+                        {isOpened &&
+                            <div>
+                                <div className="contact-number">
+                                    {email}
+                                </div>
+                                <div className="contact-number">
+                                    {address}
+                                </div>
+                            </div>}
+                    </div>
+                </li>;
     }
 }
 
@@ -55,7 +97,7 @@ class ContactList extends Component {
         };
     }
 
-    handleSearch(event) {
+    handleSearch = event => {
         let searchQuery = event.target.value.toLowerCase();
         let displayedContacts = CONTACTS.filter((el) => {
             let searchValue = el.name.toLowerCase();
@@ -66,11 +108,17 @@ class ContactList extends Component {
 
     render() {
         return <div className="contacts">
-            <input type="text" placeholder="Search..." className="search-field" onChange={this.handleSearch.bind(this)}/>
+            <input type="text" placeholder="Search..." className="search-field" onChange={this.handleSearch}/>
             <ul className="contacts-list">
                 {
                     this.state.displayedContacts.map((el) => {
-                        return <Contact key={el.id} name={el.name} phoneNumber={el.phoneNumber} image={el.image}/>;
+                        return <Contact
+                            key={el.id}
+                            name={el.name}
+                            phoneNumber={el.phoneNumber}
+                            image={el.image}
+                            email={el.email}
+                            address={el.address}/>;
                     })
                 }
             </ul>
