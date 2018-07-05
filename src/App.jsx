6bf2b2bf819c5dc2def1cb914d79cc5992fad767
Alexandr Makeev <ssh11ssh@gmail.com>
2018-07-05 18:29:13 +0300

    App Header Fix

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Makeev vs Sketch Service</h1>
+          <h1 className="App-title">History vs Sketch Service</h1>
         </header>
         <MuiThemeProvider>
           <HashRouter>
